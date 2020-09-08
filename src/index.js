eada0f841ecfb47af851f7ea0e7868e3f2fbfc58
vitalymo <vitaly.mortikov@mail.ru>
2020-09-08 13:33:06 +0300

    added steam-user module and set up auth

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-console.log('hey');
+const SteamUser = require('steam-user');
+
+const client = new SteamUser();
+const creds = require('./config.json');
+
+client.logOn(creds);
+
+client.on('loggedOn', function(details) {
+    console.log("Logged into Steam as " + client.steamID.getSteam3RenderedID());
+    client.setPersona(SteamUser.EPersonaState.Online);
+});
+
+client.on('error', function(e) {
+    // Some error occurred during logon
+    console.log(e.message);
+});
