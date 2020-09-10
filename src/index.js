7651123b7caadf2010bee0b3fda617dc5b66d3dc
vitalymo <vitaly.mortikov@mail.ru>
2020-09-10 14:17:15 +0300

    fetches the data dependently on passed time + tiny refactor

--- src/index.js
+++ src/index.js
@@ -1,60 +1,58 @@
 const invertBy = require('lodash.invertby');
 const fs = require('fs');
 const SteamUser = require('steam-user');
-
+const SteamBg = require('./modules/SteamBackground');
 const client = new SteamUser();
 const DATA_FPATH = './data.json';
 
-client.on('loggedOn', function(details) {
-
-    client.on('friendsList', async () => {
-        const actualFriends = invertBy(client.myFriends)['3']; // 3 means invitations, blocks, etc were removed
-        const sids = Object.values(actualFriends);
-
-        // check file (execute by date label inside); else - throw error
-        const data = await getBgs(sids);
-        const mostValuableHashes = Object.keys(data).filter(bgHash => data[bgHash].owners.length > 1);
-        const sorted = mostValuableHashes.map(bgHash => {
-            // data[bgHash].bgHash = bgHash;
-            return data[bgHash];
-        });
-
-        fs.writeFile(DATA_FPATH, JSON.stringify(sorted, null, 2), {flag: 'w+'}, err => {
-            if (err) throw new Error(err);
-        });
-    })
-});
-
-client.on('error', function(e) {
-    // Some error occurred during logon
-    console.log(e.message);
-});
-
 const main = () => {
     try {
-
         const creds = require('./config.json');
         client.logOn(creds);
 
+        client.on('loggedOn', ({client_supplied_steamid: sid}) => {
+            console.log(`logged in ${sid}`);
+        });
+        client.on('friendsList', async () => {
+            // get sids
+            const actualFriends = invertBy(client.myFriends)['3']; // 3 means invitations, blocks, etc were removed
+            const sids = Object.values(actualFriends);
+
+            if (!fs.existsSync(DATA_FPATH)) {
+                await fetchBgs(sids);
+            } else {
+                if (timeToUpdate(DATA_FPATH)) {
+                    await fetchBgs(sids);
+                } else {
+                    console.log(`the data had already been updated`);
+                }
+            }
+        });
+        client.on('error', e => {
+            throw new e;
+        });
     } catch (e) {
         console.log(e.message);
     }
 }
 
-const getBgLinkHash = (link) => {
-    link = link || '';
-    const template = /^https:\/\/steamcdn-a\.akamaihd\.net\/steamcommunity\/public\/images\/items\/.*\/(.*)\..*$/;
-    const matches = link.match(template);
-    return matches ? matches[1] : null;
+const fetchBgs = async (sids) => {
+    const data = await getBgs(sids);
+
+    // filter
+    const mostValuableHashes = Object.keys(data).filter(bgHash => data[bgHash].owners.length > 1);
+    const sorted = mostValuableHashes.map(bgHash => data[bgHash]);
+
+    // write
+    fs.writeFile(DATA_FPATH, JSON.stringify(sorted, null, 2), {flag: 'w+'}, err => {
+        if (err) throw new Error(err);
+        console.log(`the data file has just been fetched`);
+    });
 }
 
 const getBgs = async (sids) => {
     // get bg object by steamid
-    const promises = sids.map(async sid => {
-        const r = await client.getEquippedProfileItems(sid, {'language': 'english',});
-        r.sid = sid;
-        return r;
-    });
+    const promises = sids.map(getProfileItem);
     const response = await Promise.all(promises);
     const withBgs = response.filter(item => item.profile_background !== null);
 
@@ -70,7 +68,7 @@ const getBgs = async (sids) => {
 
         if (movie_mp4 !== null) bgLink = movie_mp4; // get animated if exists
 
-        bgHash = getBgLinkHash(bgLink);
+        bgHash = SteamBg.getLinkHash(bgLink);
 
         // check if the key of the bg obj doesn't exist
         if (!data.hasOwnProperty(bgHash)) {
@@ -85,13 +83,16 @@ const getBgs = async (sids) => {
     })
     return data;
 }
+const getProfileItem = async sid => {
+    const r = await client.getEquippedProfileItems(sid, {'language': 'english',});
+    r.sid = sid;
+    return r;
+}
 
-const needsToRefresh = (path) => {
+const timeToUpdate = (path) => {
     const {mtime} = fs.statSync(path);
     let passedHours = Math.floor((new Date().getTime() - mtime) / 1000 / 60) / 60;
     return passedHours >= 10
 }
 
 main();
-
-// test.map(item => getBgLinkHash(item.profile_background.image_large))
