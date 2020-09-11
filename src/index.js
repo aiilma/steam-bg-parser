530ca39f1b5163de12ad3e21f8a022b740fff15f
vitalymo <vitaly.mortikov@mail.ru>
2020-09-11 15:02:55 +0300

    refactor: split into independent functions #1

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-const invertBy = require('lodash.invertby');
 const fs = require('fs');
 const SteamUser = require('steam-user');
 const SteamBg = require('./modules/SteamBackground');
@@ -14,19 +13,19 @@ const main = () => {
             console.log(`logged in ${sid}`);
         });
         client.on('friendsList', async () => {
-            // get sids
-            const actualFriends = invertBy(client.myFriends)['3']; // 3 means invitations, blocks, etc were removed
-            const sids = Object.values(actualFriends);
+            const sids = mapFriendsToSids(client.myFriends, '3');
 
-            if (!fs.existsSync(DATA_FPATH)) {
-                await fetchBgs(sids);
-            } else {
-                if (timeToUpdate(DATA_FPATH)) {
-                    await fetchBgs(sids);
-                } else {
-                    console.log(`the data had already been updated`);
-                }
-            }
+            if (shouldHandle(DATA_FPATH)) {
+                const data = await getBgs(sids);
+
+                // filter
+                const hash = Object.keys(data).filter(bgHash => data[bgHash].owners.length >= 3);
+                const filtered = hash.map(bgHash => data[bgHash]);
+                // const filtered = data.filter(item => item.owners.length >= 3);
+
+                saveToFile(filtered, DATA_FPATH);
+            } else
+                console.log(`the data were fetched`);
         });
         client.on('error', e => {
             throw new e;
@@ -36,38 +35,33 @@ const main = () => {
     }
 }
 
-const fetchBgs = async (sids) => {
-    const data = await getBgs(sids);
+const mapFriendsToSids = (friends, typeId) => {
+    const invertBy = require('lodash.invertby');
 
-    // filter
-    const mostValuableHashes = Object.keys(data).filter(bgHash => data[bgHash].owners.length > 1);
-    const sorted = mostValuableHashes.map(bgHash => data[bgHash]);
+    const actualFriends = invertBy(friends)[typeId]; // 3 means invitations, blocks, etc were removed
 
-    // write
-    fs.writeFile(DATA_FPATH, JSON.stringify(sorted, null, 2), {flag: 'w+'}, err => {
-        if (err) throw new Error(err);
-        console.log(`the data file has just been fetched`);
-    });
+    return Object.values(actualFriends);
+}
+const shouldHandle = (path) => !fs.existsSync(path) || timeToUpdate(path);
+const timeToUpdate = (path) => {
+    const {mtime} = fs.statSync(path);
+    let passedHours = Math.floor((new Date().getTime() - mtime) / 1000 / 60) / 60;
+    return passedHours >= 10
 }
 
 const getBgs = async (sids) => {
-    // get bg object by steamid
-    const promises = sids.map(getProfileItem);
-    const response = await Promise.all(promises);
-    const withBgs = response.filter(item => item.profile_background !== null);
+    const pItems = await getProfileItems(sids);
 
     const data = {};
-    withBgs.forEach(item => {
+    pItems.forEach(item => {
         let bgHash, {
-            sid,
-            profile_background: {
+            sid, profile_background: {
                 movie_mp4,
                 image_large: bgLink,
             },
         } = item;
 
         if (movie_mp4 !== null) bgLink = movie_mp4; // get animated if exists
-
         bgHash = SteamBg.getLinkHash(bgLink);
 
         // check if the key of the bg obj doesn't exist
@@ -83,16 +77,28 @@ const getBgs = async (sids) => {
     })
     return data;
 }
-const getProfileItem = async sid => {
-    const r = await client.getEquippedProfileItems(sid, {'language': 'english',});
-    r.sid = sid;
-    return r;
+const getProfileItems = async (sids) => {
+    const hasBackground = item => item.profile_background !== null;
+    const getProfileItem = async sid => {
+        const r = await client.getEquippedProfileItems(sid, {'language': 'english',});
+        r.sid = sid;
+        return r;
+    }
+
+    const promises = sids.map(getProfileItem);
+    const response = await Promise.all(promises);
+
+    return response.filter(hasBackground);
 }
 
-const timeToUpdate = (path) => {
-    const {mtime} = fs.statSync(path);
-    let passedHours = Math.floor((new Date().getTime() - mtime) / 1000 / 60) / 60;
-    return passedHours >= 10
+const saveToFile = (data, path) => {
+    const dataStr = JSON.stringify(data, null, 2);
+
+    fs.writeFile(path, dataStr, {flag: 'w+'}, err => {
+        if (err) throw new Error(err);
+        console.log(`the data file has just been fetched`);
+    });
 }
+
 
 main();
